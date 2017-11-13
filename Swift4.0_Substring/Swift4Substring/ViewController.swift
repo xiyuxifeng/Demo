2b729000241974c8822a8de0bb4516509a7150f7
Wang Hui <1014319350@qq.com>
2017-11-13 22:20:06 +0800

    更改目录

---
+++ Swift4.0_Substring/Swift4Substring/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift4Substring
+//
+//  Created by 王辉 on 2017/11/13.
+//  Copyright © 2017年 wanghui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Swift4.0_Substring/Swift4SubstringTests/Swift4SubstringTests.swift
@@ -0,0 +1,36 @@
+//
+//  Swift4SubstringTests.swift
+//  Swift4SubstringTests
+//
+//  Created by 王辉 on 2017/11/13.
+//  Copyright © 2017年 wanghui. All rights reserved.
+//
+
+import XCTest
+@testable import Swift4Substring
+
+class Swift4SubstringTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
